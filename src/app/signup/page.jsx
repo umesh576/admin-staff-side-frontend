e1077f187a28d8b37fa8bdbba8246e0a04f5fb25
Umesh Joshi <uj19343@gmail.com>
2025-09-02 22:18:29 +0545

    new chnages on device

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -206,7 +206,7 @@ const SignupPage = () => {
             </form>
           </div>
           <div>
-            <button>Sign up somethign with google</button>
+            <button>Sign up with google</button>
           </div>
         </div>
       </div>
